2fc0155093c21b059ebe778615ce201d7b21828e
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-11-07 19:29:17 +0300

    create missingInteger function that finds and returns the missing int from the array

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,29 @@
 import java.io.Console;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws IOException {
 
+    }
+
+    public static  int missingInteger(int[] arr) {
+        //sort the array so as to loop through it
+        Arrays.sort(arr);
+
+        int missingInt = 0;
+
+        //initialize integer that increments each time the array is looped through
+        // so as to equate with each array int to find missing number
+        int count = 0;
+        for (int value : arr) {
+            //increment count by 1 then check if value is not equals to count, then count is the missing integer
+            count += 1;
+            if (count != value) {
+                missingInt = count;
+            }
+        }
+
+        return missingInt;
     }
 }
