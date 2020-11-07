8660fcdc3fdd89cbc1a395711840c1ae1b935345
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-11-07 20:41:48 +0300

    Add code to main function to take in the array and return the missing int via console

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,35 @@
 import java.io.Console;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException {
 
+        //taking int array length from user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Length of int array");
+        int arrLength = sc.nextInt();
+
+        // create int array
+        int[] integerInputArray = new int[arrLength];
+
+        //fetch user input to save into the created array
+        System.out.println("Please enter array elements of type int");
+        for (int i = 0; i < arrLength; i++) {
+            int userInput = sc.nextInt();
+            integerInputArray[i] = userInput;
+        }
+
+        System.out.println("The inputted int array is: ");
+        System.out.println(integerInputArray);
+        if (missingInteger(integerInputArray) == 0){
+            System.out.println("There is no missing integer in your array");
+        }
+        else {
+            System.out.println("The missing integer is :");
+            System.out.println(missingInteger(integerInputArray));
+        }
     }
 
     public static  int missingInteger(int[] arr) {
